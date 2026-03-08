b45145c9f3ffed952ebb9e4de7cf510eab91b653
ʊɨ_աɛɮɦǟʀʀʏ⚡ <ifebhorharrison@gmail.com>
2026-03-08 14:13:19 -0700

    Third commit

--- server.ts
+++ server.ts
@@ -23,7 +23,7 @@ db.exec(`
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = Number(process.env.PORT) || 3000;
 
   app.use(cors()); // Enable CORS for all routes
   app.use(express.json({ limit: '10mb' }));
